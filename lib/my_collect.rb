39dfcc41738afd3db34e54be5f9c3b332895e55c
Matt Maher <mattmaher0@gmail.com>
2018-05-15 14:17:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,16 @@ def my_collect(array)
   count = 0
   new_array = []
   while count < array.length
+    if array == "students"
+      yield array[count]
+     new_array << array[count].split(" ").first
+    elsif array == "languages"
+      yield array[count]
+     new_array << array[count].upcase
+    else
      yield array[count]
      new_array << array[count]
+    end
   count += 1
   end
   new_array
