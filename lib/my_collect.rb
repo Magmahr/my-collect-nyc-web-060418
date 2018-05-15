ca660737e99924ff42f8f8dc60baba6d06504490
Matt Maher <mattmaher0@gmail.com>
2018-05-15 14:10:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ def my_collect(array)
   count = 0
   new_array = []
   while count < array.length
-  new_array << yield array[count]
+    new_array << yield array[count]
+  count += 1
+  end
   new_array
 end  
 
